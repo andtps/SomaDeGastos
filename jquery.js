a15174cb8b4b2326e2e07d680a2b5bbb61d278e2
André Moreira <tpsandre19@gmail.com>
2024-01-11 17:44:58 -0300

    adicionar arquivos no repositório git

---
+++ jquery.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    
+    const InputAuguel = $('#aluguel').val();
+    const InputEscola = $('#escola').val();
+    const InputCar = $('#car').val();
+    const InputAgua = $('#agua').val();
+    const InputEnergia = $('#energia').val();
+    const InputMercado = $('#mercado').val();
+    
+
+
+});
